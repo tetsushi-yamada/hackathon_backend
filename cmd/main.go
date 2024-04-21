9cb808f83c70950e187671602ec0e2de3a6f90d9
tetsushi-yamada <104970929+tetsushi-yamada@users.noreply.github.com>
2024-04-21 18:58:06 +0900

    edit main

--- cmd/main.go
+++ cmd/main.go
@@ -8,15 +8,14 @@ import (
 )
 
 func main() {
-	router := server.NewRouter()
-	err := http.ListenAndServe(":8080", router)
-	if err != nil {
-		log.Fatalf("Server failed to start: %v", err)
-	}
-
 	db := init_query.StartDB()
-	err = init_query.Init_table(db)
+	err := init_query.Init_table(db)
 	if err != nil {
 		log.Fatal(err)
+	}
+	router := server.NewRouter()
+	err = http.ListenAndServe(":8080", router)
+	if err != nil {
+		log.Fatalf("Server failed to start: %v", err)
 	}
 }
