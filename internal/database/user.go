a4558128d4b79b76e3308a768c4341184ea6963a
tetsushi-yamada <104970929+tetsushi-yamada@users.noreply.github.com>
2024-04-22 14:20:06 +0900

    edit time

--- internal/database/user.go
+++ internal/database/user.go
@@ -3,6 +3,7 @@ package database
 import (
 	"database/sql"
 	"github.com/tetsushi-yamada/hackathon_backend/internal/domain"
+	"time"
 )
 
 type UserDatabase struct{}
@@ -18,7 +19,16 @@ func (repo *UserDatabase) CreateUserTx(tx *sql.Tx, user domain.User) error {
 func (repo *UserDatabase) GetUserTx(tx *sql.Tx, userID string) (*domain.User, error) {
 	user := new(domain.User)
 	query := `SELECT user_id, user_name, email, created_at, updated_at FROM users WHERE user_id = ?`
-	err := tx.QueryRow(query, userID).Scan(&user.UserId, &user.UserName, &user.Email, &user.CreatedAt, &user.UpdatedAt)
+	var createdAt, updatedAt []byte
+	err := tx.QueryRow(query, userID).Scan(&user.UserId, &user.UserName, &user.Email, &createdAt, &updatedAt)
+	if err != nil {
+		return nil, err
+	}
+	user.CreatedAt, err = time.Parse("2006-01-02 15:04:05", string(createdAt))
+	if err != nil {
+		return nil, err
+	}
+	user.UpdatedAt, err = time.Parse("2006-01-02 15:04:05", string(updatedAt))
 	if err != nil {
 		return nil, err
 	}
